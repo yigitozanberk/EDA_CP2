586c793fe84f2cdab391390ca4fa78f2b6daf25c
YOB <yigitozanberk@gmail.com>
2019-05-09 22:44:43 +0300

    eda cp2 3 - 4

--- plot3.R
+++ plot3.R
@@ -10,18 +10,33 @@ SCC <- readRDS("Source_Classification_Code.rds")
 #subset for baltimore city
 mn0 <- subset(NEI, fips == "24510")
 
-#means of different types on yearly basis
-mn1 <- with(mn0, tapply(Emissions, list(year, type), mean))
+#mark sources with emissions greater than 20 tons per year
+mn1 <- mutate(mn0, treshold = Emissions > 20)
+
+#take mean values accordingly
+mn2 <- with(mn1, tapply(Emissions, list(year, type, treshold), mean))
 #melt the data into tidy data
-mt <- melt(mn1)
-colnames(mt) <- c("year", "type", "value")
+mt <- melt(mn2)
+colnames(mt) <- c("year", "type", "treshold_ID" ,"value")
+
+#Total emissions
+sm0 <- with(mn0, tapply(Emissions, year, sum, na.rm = T))
+sm1 <- paste(as.integer(sm0), "Tons|")
+cap <- paste("1999 Total:", sm1[1], "2002 Total:", sm1[2], "2005 Total:", 
+             sm1[3], "2008 Total:", 
+             sm1[4], sep = " ")
+
+
+
 
 #plot the data of average yearly emissions of pm2.5 in Baltimore City by
 # different types of emittors
 g <- ggplot(mt, aes(year, value))
-g <- g + geom_point() + facet_grid(.~ type) 
+g <- g + geom_point() + facet_grid(treshold_ID ~ type, scales = "free_y") 
 g <- g + geom_smooth(method = "lm") 
-g <- g + labs(title = "Average Yearly Emissions of PM2.5 in Baltimore")
+g <- g + labs(title = "Average Yearly Emissions of PM2.5 in Baltimore", 
+              subtitle = "Sources of Above(Top Row) and Below(Bottom Row) 20 Tons per Year",
+              caption = cap) + ylab("PM2.5 Emission in Tons")
 print(g)
 dev.copy(png, file = "plot3.png")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,6 +1,7 @@
 #plot4.R
 #initiation
 library(ggplot2)
+library(dplyr)
 library(reshape2)
 #read data
 NEI <- readRDS("summarySCC_PM25.rds")
@@ -12,14 +13,73 @@ cdata <- unique(as.character(SCC$SCC[grepl("Coal", SCC$EI.Sector)]))
 #subset the actual data
 coal <- subset(NEI, SCC %in% cdata)
 
-#means of emissions by year
-mn0 <- melt(with(coal, tapply(Emissions, year, mean)))
-colnames(mn0) <- c("year", "average_emission")
+#total emission each year
+sm0 <- with(cl1, tapply(Emissions, year, sum, na.rm = T))
+sm1 <- paste(as.integer(sm0), "Tons|")
+cap <- paste("1999 Total:", sm1[1], "2002 Total:", sm1[2], "2005 Total:", 
+             sm1[3], "2008 Total:", 
+             sm1[4], sep = " ")
 
-# the plot of average emission of coal cumbustion related sources by year
-g <- ggplot(mn0, aes(year, average_emission))
-g <- g + geom_point() + geom_smooth(method = "lm")
-g <- g + labs(title = "Average Emission of Coal Cumbustion Related Sources")
-print(g)
+#look at unique sources
+yr1 <- unique(subset(coal, year == 1999, c(fips, SCC)))
+yr2 <- unique(subset(coal, year == 2002, c(fips, SCC)))
+yr3 <- unique(subset(coal, year == 2005, c(fips, SCC)))
+yr4 <- unique(subset(coal, year == 2008, c(fips, SCC)))
+
+yr1 <- paste(yr1[, 1], yr1[, 2], sep = ".")
+yr2 <- paste(yr2[, 1], yr2[, 2], sep = ".")
+yr3 <- paste(yr3[, 1], yr3[, 2], sep = ".")
+yr4 <- paste(yr4[, 1], yr4[, 2], sep = ".")
+
+#fips.SCC combinations observed in all years
+all1 <- intersect(yr1, yr2)
+all2 <- intersect(yr3, yr4)
+all4 <- intersect(all1, all2)
+
+coal$fips.SCC <- with(coal, paste(fips, SCC, sep = "."))
+c1 <- subset(coal, year == 1999 & fips.SCC %in% all4)
+c2 <- subset(coal, year == 2002 & fips.SCC %in% all4)
+c3 <- subset(coal, year == 2005 & fips.SCC %in% all4)
+c4 <- subset(coal, year == 2008 & fips.SCC %in% all4) #there are duplicate rows
+c5 <- aggregate(Emissions ~ fips.SCC + fips + SCC + Pollutant + type + year,data= c4,FUN=sum)
+
+c1fips <- aggregate(Emissions ~ fips, data = c1, FUN = sum)
+c2fips <- aggregate(Emissions ~ fips, data = c2, FUN = sum)
+c3fips <- aggregate(Emissions ~ fips, data = c3, FUN = sum)
+c4fips <- aggregate(Emissions ~ fips, data = c5, FUN = sum)
+
+ctop <- merge(c1fips, c2fips, by = "fips")
+ctop <- merge(ctop, c3fips, by = "fips")
+colnames(ctop) <- c("fips", "Emissions1999", "Emissions2002", "Emissions2005")
+ctop <- merge(ctop, c4fips, by = "fips")
+colnames(ctop) <- c("fips", "Emissions1999", "Emissions2002", "Emissions2005",
+                    "Emissions2008")
+
+#total observations
+tot <- aggregate(Emissions ~ year, data = coal, FUN = sum)
+
+#plotting of prevalent county&source wise plots and overall changes
+par(mfrow = c(1,2), mar = c(6, 5, 2, 1), oma = c(0, 0, 2, 0))
+with(ctop, plot(rep(1999, 1103), ctop[, 2], xlim = c(1998, 2010), 
+                ylim = c(0, 28000), 
+                main = "County-Wise Emission",
+                sub = "Coal Combustion-Related Sources",
+                xlab = "Years 1999 - 2008",
+                ylab = "PM2.5 Emission in Tons"))
+with(ctop, points(rep(2002, 1103), ctop[, 3]))
+with(ctop, points(rep(2005, 1103), ctop[, 4]))
+with(ctop, points(rep(2008, 1103), ctop[, 5]))
+segments(rep(1999, 1103), ctop[,2], rep(2002, 1103), ctop[,3],
+         col = rgb(red = 0, green = 0, blue = 1, alpha = 0.2))
+segments(rep(2002, 1103), ctop[,3], rep(2005, 1103), ctop[,4],
+         col = rgb(red = 0, green = 0, blue = 1, alpha = 0.2))
+segments(rep(2005, 1103), ctop[,4], rep(2008, 1103), ctop[,5],
+         col = rgb(red = 0, green = 0, blue = 1, alpha = 0.2))
+mtext(cap, outer = TRUE, cex = 0.8)
+with(tot, plot(year, Emissions, xlab = "Years 1999 - 2008",
+               ylab = "PM2.5 Emission in Tons",
+               sub = "Yearly Total Country Emissions"))
+abline(lm(Emissions ~ year, tot))
+
 dev.copy(png, file = "plot4.png")
 dev.off()
